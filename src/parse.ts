ac79f174dc9638f726f2cfc1257c142435e1e1df
James Garbutt <43081j@users.noreply.github.com>
2024-06-20 12:03:06 +0100

    feat: rework to use fast-querystring (#1)
    Uses a fork of fast-querystring instead of having to do two passes.

--- src/parse.ts
+++ src/parse.ts
@@ -1,38 +1,17 @@
-import {type CONTINUE, type ArraySyntax} from './shared.js';
-import {parse as fastParse} from 'fast-querystring';
+import {
+  type ParseOptions,
+  type DeserializeKeyFunction,
+  type DeserializeValueFunction
+} from './shared.js';
+import fastDecode from 'fast-decode-uri-component';
+import {dset} from 'dset';
+import dlv from 'dlv';
 
 export type ParsedQuery = Record<PropertyKey, unknown>;
-
-export type DeserializeValueFunction = (
-  key: string,
-  value: string
-) => unknown | typeof CONTINUE;
-
-export type DeserializeKeyFunction = (
-  key: string
-) => PropertyKey | typeof CONTINUE;
-
-export interface ParseOptions {
-  // Enable parsing nested objects and arrays
-  // default: true
-  nested: boolean;
-
-  // Array syntax
-  // default: "index"
-  arraySyntax: ArraySyntax;
-
-  // Delimiter to split kv pairs by
-  // default: "&"
-  delimiter: string;
-
-  // Custom deserializers
-  valueDeserializer: DeserializeValueFunction;
-  keyDeserializer: DeserializeKeyFunction;
-}
-
 export type UserParseOptions = Partial<ParseOptions>;
+type DlvKey = string | (string | number)[];
 
-const defaultKeyDeserializer: DeserializeKeyFunction = (key) => {
+export const numberKeyDeserializer: DeserializeKeyFunction = (key) => {
   const asNumber = Number(key);
   if (!Number.isNaN(asNumber)) {
     return asNumber;
@@ -40,7 +19,10 @@ const defaultKeyDeserializer: DeserializeKeyFunction = (key) => {
   return key;
 };
 
-const defaultValueDeserializer: DeserializeValueFunction = (_key, value) => {
+export const numberValueDeserializer: DeserializeValueFunction = (
+  _key,
+  value
+) => {
   const asNumber = Number(value);
   if (!Number.isNaN(asNumber)) {
     return asNumber;
@@ -50,38 +32,201 @@ const defaultValueDeserializer: DeserializeValueFunction = (_key, value) => {
 
 const defaultOptions: ParseOptions = {
   nested: true,
-  arraySyntax: 'repeat',
-  delimiter: '&',
-  valueDeserializer: defaultValueDeserializer,
-  keyDeserializer: defaultKeyDeserializer
+  nestingSyntax: 'dot',
+  arrayRepeat: false,
+  arrayRepeatSyntax: 'repeat',
+  delimiter: 38
 };
 
-export function parse(
-  input: string,
-  options?: Partial<ParseOptions>
-): ParsedQuery {
-  const mergedOptions = {...defaultOptions, ...options};
-  const getKey = mergedOptions.keyDeserializer;
-  const getValue = mergedOptions.valueDeserializer;
+function splitByIndexPattern(input: string): string[] {
+  const result: string[] = [];
+  let buffer = '';
 
-  const parsed: Record<string, unknown> = fastParse(input);
-  const result: ParsedQuery = {};
+  for (let i = 0; i < input.length; i++) {
+    const chr = input[i];
+    const nextChr = input[i + 1];
+    if (chr === '[' || chr === ']') {
+      result.push(buffer);
+      buffer = '';
+      if (nextChr === '[') {
+        i++;
+      }
+    } else {
+      buffer += chr;
+    }
+  }
 
-  for (const key in parsed) {
-    if (Object.prototype.hasOwnProperty.call(parsed, key)) {
-      const value = parsed[key];
+  if (buffer) {
+    result.push(buffer);
+  }
 
-      if (Array.isArray(value)) {
-        result[getKey(key)] = value.map((v) => {
-          if (typeof v === 'string') {
-            return getValue('[]', v);
+  return result;
+}
+
+const regexPlus = /\+/g;
+const Empty = function () {} as unknown as {new (): ParsedQuery};
+Empty.prototype = Object.create(null);
+
+/**
+ * Parses a query string into an object
+ * @param {string} input
+ * @param {ParseOptions=} options
+ */
+export function parse(input: string, options?: UserParseOptions): ParsedQuery {
+  const parseOptions: ParseOptions = {...defaultOptions, ...options};
+  const charDelimiter =
+    typeof parseOptions.delimiter === 'string'
+      ? parseOptions.delimiter.charCodeAt(0)
+      : parseOptions.delimiter;
+  const {
+    valueDeserializer,
+    keyDeserializer,
+    arrayRepeatSyntax,
+    nested,
+    arrayRepeat,
+    nestingSyntax
+  } = parseOptions;
+
+  // Optimization: Use new Empty() instead of Object.create(null) for performance
+  // v8 has a better optimization for initializing functions compared to Object
+  const result = new Empty();
+
+  if (typeof input !== 'string') {
+    return result;
+  }
+
+  const inputLength = input.length;
+  let key = '';
+  let value = '';
+  let startingIndex = -1;
+  let equalityIndex = -1;
+  let shouldDecodeKey = false;
+  let shouldDecodeValue = false;
+  let keyHasPlus = false;
+  let valueHasPlus = false;
+  let hasBothKeyValuePair = false;
+  let c = 0;
+
+  // Have a boundary of input.length + 1 to access last pair inside the loop.
+  for (let i = 0; i < inputLength + 1; i++) {
+    c = i !== inputLength ? input.charCodeAt(i) : charDelimiter;
+
+    // Handle '&' and end of line to pass the current values to result
+    if (c === charDelimiter) {
+      hasBothKeyValuePair = equalityIndex > startingIndex;
+
+      // Optimization: Reuse equality index to store the end of key
+      if (!hasBothKeyValuePair) {
+        equalityIndex = i;
+      }
+
+      key = input.slice(startingIndex + 1, equalityIndex);
+
+      // Add key/value pair only if the range size is greater than 1; a.k.a. contains at least "="
+      if (hasBothKeyValuePair || key.length > 0) {
+        // Optimization: Replace '+' with space
+        if (keyHasPlus) {
+          key = key.replace(regexPlus, ' ');
+        }
+
+        // Optimization: Do not decode if it's not necessary.
+        if (shouldDecodeKey) {
+          key = fastDecode(key) || key;
+        }
+
+        if (hasBothKeyValuePair) {
+          value = input.slice(equalityIndex + 1, i);
+
+          if (valueHasPlus) {
+            value = value.replace(regexPlus, ' ');
           }
-          return v;
-        });
-      } else if (typeof value === 'string') {
-        result[getKey(key)] = getValue(key, value);
+
+          if (shouldDecodeValue) {
+            value = fastDecode(value) || value;
+          }
+        }
+
+        if (
+          arrayRepeat &&
+          arrayRepeatSyntax === 'bracket' &&
+          key.endsWith('[]')
+        ) {
+          key = key.slice(0, -2);
+        }
+
+        const newValue = valueDeserializer
+          ? valueDeserializer(key, value)
+          : value;
+        const newKey = keyDeserializer ? keyDeserializer(key) : key;
+        let dlvKey = (typeof newKey === 'string' ? newKey : [newKey]) as DlvKey;
+
+        if (typeof dlvKey === 'string' && nested) {
+          if (nestingSyntax === 'index') {
+            dlvKey = splitByIndexPattern(dlvKey);
+          } else {
+            dlvKey = dlvKey.split('.');
+          }
+        }
+
+        const shouldDoNesting =
+          nested && (dlvKey as string[]).pop !== undefined;
+        const currentValue = shouldDoNesting
+          ? dlv(result, dlvKey)
+          : result[newKey];
+
+        if (currentValue === undefined || !arrayRepeat) {
+          if (shouldDoNesting) {
+            dset(result, dlvKey, newValue);
+          } else {
+            result[newKey] = newValue;
+          }
+        } else if (arrayRepeat) {
+          // Optimization: value.pop is faster than Array.isArray(value)
+          if ((currentValue as unknown[]).pop) {
+            (currentValue as unknown[]).push(newValue);
+          } else {
+            if (shouldDoNesting) {
+              dset(result, newKey as string, [currentValue, newValue]);
+            } else {
+              result[newKey] = [currentValue, newValue];
+            }
+          }
+        }
+      }
+
+      // Reset reading key value pairs
+      value = '';
+      startingIndex = i;
+      equalityIndex = i;
+      shouldDecodeKey = false;
+      shouldDecodeValue = false;
+      keyHasPlus = false;
+      valueHasPlus = false;
+    }
+    // Check '='
+    else if (c === 61) {
+      if (equalityIndex <= startingIndex) {
+        equalityIndex = i;
+      }
+      // If '=' character occurs again, we should decode the input.
+      else {
+        shouldDecodeValue = true;
+      }
+    }
+    // Check '+', and remember to replace it with empty space.
+    else if (c === 43) {
+      if (equalityIndex > startingIndex) {
+        valueHasPlus = true;
       } else {
-        result[getKey(key)] = value;
+        keyHasPlus = true;
+      }
+    }
+    // Check '%' character for encoding
+    else if (c === 37) {
+      if (equalityIndex > startingIndex) {
+        shouldDecodeValue = true;
+      } else {
+        shouldDecodeKey = true;
       }
     }
   }
--- src/shared.ts
+++ src/shared.ts
@@ -1,14 +1,52 @@
-export type ArraySyntax =
-  // `foo[0]=a&foo[1]=b`
-  | 'index'
-  // same as `index` but `foo[0]=a&foo[2]=b` would be ['a', undefined, 'b']
-  | 'index-sparse'
+export type ArrayRepeatSyntax =
   // `foo[]=a&foo[]=b`
   | 'bracket'
   // `foo=a&foo=b`
   | 'repeat';
 
+export type NestingSyntax =
+  // `foo.bar`
+  | 'dot'
+  // `foo[bar]`
+  | 'index';
+
 // This is a special return value for deserializers and serializers, to tell the library
 // to fall back to using the default (de)serialize function.
 // We can't just return `null` or `undefined` etc, because we may want to deserialize to that
 export const CONTINUE = Symbol('continue');
+
+export type DeserializeValueFunction = (
+  key: string,
+  value: string
+) => unknown | typeof CONTINUE;
+
+export type DeserializeKeyFunction = (
+  key: string
+) => PropertyKey | typeof CONTINUE;
+
+export interface ParseOptions {
+  // Enable parsing nested objects and arrays
+  // default: true
+  nested: boolean;
+
+  // Nesting syntax
+  // default: "index"
+  nestingSyntax: NestingSyntax;
+
+  // Whether repeated keys should result in arrays
+  // e.g. `foo=1&foo=2` would become `foo: [1, 2]`
+  // default: false
+  arrayRepeat: boolean;
+
+  // Array syntax
+  // default: "repeat"
+  arrayRepeatSyntax: ArrayRepeatSyntax;
+
+  // Delimiter to split kv pairs by
+  // default: "&"
+  delimiter: string | number;
+
+  // Custom deserializers
+  valueDeserializer?: DeserializeValueFunction;
+  keyDeserializer?: DeserializeKeyFunction;
+}
