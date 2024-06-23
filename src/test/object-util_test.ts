ddf19a3df6f62d2808c2860f8ebef9794469e6f4
James Garbutt <43081j@users.noreply.github.com>
2024-06-23 17:07:15 +0100

    perf: rework stringify to avoid array alloc (#25)
    Changes the stringify logic to build a string up internally rather than
    producing an array of traversed key/value pairs.
    
    Should result in a little less memory usage and seems to bump the perf
    up a fair amount in some cases.

--- src/test/object-util_test.ts
+++ src/test/object-util_test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'node:assert/strict';
 import {test} from 'node:test';
-import {getNestedValues, getDeepObject} from '../object-util.js';
+import {stringifyObject, getDeepObject} from '../object-util.js';
 
 test('getDeepObject', async (t) => {
   await t.test('object if string key', () => {
@@ -50,16 +50,13 @@ test('getDeepObject', async (t) => {
   });
 });
 
-test('getNestedValues', async (t) => {
+test('stringifyObject', async (t) => {
   await t.test('shallow values', () => {
     const obj = {
       a: 'foo',
       b: 'bar'
     };
-    assert.deepEqual(getNestedValues(obj, {nestingSyntax: 'dot'}), [
-      ['a', 'foo'],
-      ['b', 'bar']
-    ]);
+    assert.equal(stringifyObject(obj, {nestingSyntax: 'dot'}), 'a=foo&b=bar');
   });
 
   await t.test('deep values', () => {
@@ -70,9 +67,7 @@ test('getNestedValues', async (t) => {
         }
       }
     };
-    assert.deepEqual(getNestedValues(obj, {nestingSyntax: 'dot'}), [
-      ['a.b.c', 'foo']
-    ]);
+    assert.deepEqual(stringifyObject(obj, {nestingSyntax: 'dot'}), 'a.b.c=foo');
   });
 
   await t.test('mixed deep and shallow values', () => {
@@ -88,12 +83,10 @@ test('getNestedValues', async (t) => {
         d1: 'foo'
       }
     };
-    assert.deepEqual(getNestedValues(obj, {nestingSyntax: 'dot'}), [
-      ['a', 'foo'],
-      ['b.b1.b2', 'foo'],
-      ['c', 'foo'],
-      ['d.d1', 'foo']
-    ]);
+    assert.deepEqual(
+      stringifyObject(obj, {nestingSyntax: 'dot'}),
+      'a=foo&b.b1.b2=foo&c=foo&d.d1=foo'
+    );
   });
 
   await t.test('deep values with multiple keys', () => {
@@ -103,20 +96,20 @@ test('getNestedValues', async (t) => {
         a2: 'bar'
       }
     };
-    assert.deepEqual(getNestedValues(obj, {nestingSyntax: 'dot'}), [
-      ['a.a1', 'foo'],
-      ['a.a2', 'bar']
-    ]);
+    assert.deepEqual(
+      stringifyObject(obj, {nestingSyntax: 'dot'}),
+      'a.a1=foo&a.a2=bar'
+    );
   });
 
   await t.test('simple array', () => {
     const obj = {
       a: ['foo', 'bar']
     };
-    assert.deepEqual(getNestedValues(obj, {nestingSyntax: 'dot'}), [
-      ['a.0', 'foo'],
-      ['a.1', 'bar']
-    ]);
+    assert.deepEqual(
+      stringifyObject(obj, {nestingSyntax: 'dot'}),
+      'a.0=foo&a.1=bar'
+    );
   });
 
   await t.test('array of objects', () => {
@@ -127,9 +120,7 @@ test('getNestedValues', async (t) => {
         }
       ]
     };
-    assert.deepEqual(getNestedValues(obj, {nestingSyntax: 'dot'}), [
-      ['a.0.b', 'foo']
-    ]);
+    assert.deepEqual(stringifyObject(obj, {nestingSyntax: 'dot'}), 'a.0.b=foo');
   });
 
   await t.test('simple index syntax', () => {
@@ -141,10 +132,10 @@ test('getNestedValues', async (t) => {
         }
       }
     };
-    assert.deepEqual(getNestedValues(obj, {nestingSyntax: 'index'}), [
-      ['a[a1]', 'foo'],
-      ['a[a2][a3]', 'foo']
-    ]);
+    assert.deepEqual(
+      stringifyObject(obj, {nestingSyntax: 'index'}),
+      'a%5Ba1%5D=foo&a%5Ba2%5D%5Ba3%5D=foo'
+    );
   });
 
   await t.test('handles circular references', () => {
@@ -157,7 +148,7 @@ test('getNestedValues', async (t) => {
     obj.foo.foo2 = obj;
 
     // as long as it doesn't hang forever, we're good
-    assert.ok(getNestedValues(obj, {nestingSyntax: 'dot'}));
+    assert.ok(stringifyObject(obj, {nestingSyntax: 'dot'}));
   });
 
   await t.test('repeated array values (repeat)', () => {
@@ -165,11 +156,8 @@ test('getNestedValues', async (t) => {
       foo: [1, 2]
     };
     assert.deepEqual(
-      getNestedValues(obj, {arrayRepeat: true, arrayRepeatSyntax: 'repeat'}),
-      [
-        ['foo', 1],
-        ['foo', 2]
-      ]
+      stringifyObject(obj, {arrayRepeat: true, arrayRepeatSyntax: 'repeat'}),
+      'foo=1&foo=2'
     );
   });
 
@@ -178,15 +166,12 @@ test('getNestedValues', async (t) => {
       foo: [1, 2]
     };
     assert.deepEqual(
-      getNestedValues(obj, {arrayRepeat: true, arrayRepeatSyntax: 'bracket'}),
-      [
-        ['foo[]', 1],
-        ['foo[]', 2]
-      ]
+      stringifyObject(obj, {arrayRepeat: true, arrayRepeatSyntax: 'bracket'}),
+      'foo%5B%5D=1&foo%5B%5D=2'
     );
   });
 
-  await t.test('null values result in empty set', () => {
-    assert.deepEqual(getNestedValues(null as unknown as object, {}), []);
+  await t.test('null values result in empty string', () => {
+    assert.deepEqual(stringifyObject({foo: null}, {}), 'foo=');
   });
 });
