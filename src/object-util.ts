951f70164296016a5c7eaf7e6ec19dfd565164d1
43081j <43081j@users.noreply.github.com>
2024-06-23 17:04:35 +0100

    perf: rework stringify to avoid array alloc
    Changes the stringify logic to build a string up internally rather than
    producing an array of traversed key/value pairs.
    
    Should result in a little less memory usage and seems to bump the perf
    up a fair amount in some cases.

--- src/object-util.ts
+++ src/object-util.ts
@@ -1,4 +1,5 @@
 import {type Options, defaultOptions} from './shared.js';
+import {encodeString} from './string-util.js';
 
 type KeyableObject = Record<PropertyKey, unknown>;
 
@@ -35,26 +36,50 @@ const strBracketLeft = '[';
 const strBracketRight = ']';
 const strDot = '.';
 
-export type KeyValuePair = [PropertyKey, unknown];
+type Primitive = number | string | boolean;
 
-function walkNestedValues(
+function getAsPrimitive(value: unknown): Primitive {
+  switch (typeof value) {
+    case 'string':
+      // Length check is handled inside encodeString function
+      return encodeString(value);
+    case 'bigint':
+    case 'boolean':
+      return '' + value;
+    case 'number':
+      if (Number.isFinite(value)) {
+        return value < 1e21 ? '' + value : encodeString('' + value);
+      }
+      break;
+  }
+
+  return '';
+}
+
+export function stringifyObject(
   obj: Record<PropertyKey, unknown>,
   options: Partial<Options>,
-  out: KeyValuePair[],
   depth: number = 0,
   parentKey?: string,
   useArrayRepeatKey?: boolean
-): void {
+): string {
   const {
     nestingSyntax = defaultOptions.nestingSyntax,
     arrayRepeat = defaultOptions.arrayRepeat,
     arrayRepeatSyntax = defaultOptions.arrayRepeatSyntax,
-    nesting = defaultOptions.nesting
+    nesting = defaultOptions.nesting,
+    delimiter = defaultOptions.delimiter
   } = options;
+  const strDelimiter =
+    typeof delimiter === 'number' ? String.fromCharCode(delimiter) : delimiter;
 
   if (depth > MAX_DEPTH) {
-    return;
+    return '';
   }
+
+  let result = '';
+  let firstKey = true;
+  let probableArray = false;
 
   for (const key in obj) {
     const value = obj[key];
@@ -77,38 +102,32 @@ function walkNestedValues(
       path = key;
     }
 
-    const probableArray = (value as unknown[]).pop !== undefined;
+    if (!firstKey) {
+      result += strDelimiter;
+    }
 
-    if (
-      typeof value === 'object' &&
-      value !== null &&
-      (nesting || (arrayRepeat && probableArray))
-    ) {
-      walkNestedValues(
-        value as Record<PropertyKey, unknown>,
-        options,
-        out,
-        depth + 1,
-        path,
-        arrayRepeat && probableArray
-      );
+    if (typeof value === 'object' && value !== null) {
+      probableArray = (value as unknown[]).pop !== undefined;
+
+      if (nesting || (arrayRepeat && probableArray)) {
+        result += stringifyObject(
+          value as Record<PropertyKey, unknown>,
+          options,
+          depth + 1,
+          path,
+          arrayRepeat && probableArray
+        );
+      }
     } else {
-      out.push([path, value]);
+      result += encodeString(path);
+      result += '=';
+      result += getAsPrimitive(value);
+    }
+
+    if (firstKey) {
+      firstKey = false;
     }
   }
-}
-
-export function getNestedValues(
-  obj: object,
-  options: Partial<Options>
-): KeyValuePair[] {
-  const result: KeyValuePair[] = [];
-
-  if (obj === null) {
-    return result;
-  }
-
-  walkNestedValues(obj as Record<PropertyKey, unknown>, options, result);
 
   return result;
 }
--- src/stringify.ts
+++ src/stringify.ts
@@ -1,26 +1,5 @@
-import {encodeString} from './string-util.js';
-import {type Options, defaultOptions} from './shared.js';
-import {getNestedValues} from './object-util.js';
-
-type Primitive = number | string | boolean;
-
-function getAsPrimitive(value: unknown): Primitive {
-  switch (typeof value) {
-    case 'string':
-      // Length check is handled inside encodeString function
-      return encodeString(value);
-    case 'bigint':
-    case 'boolean':
-      return '' + value;
-    case 'number':
-      if (Number.isFinite(value)) {
-        return value < 1e21 ? '' + value : encodeString('' + value);
-      }
-      break;
-  }
-
-  return '';
-}
+import {type Options} from './shared.js';
+import {stringifyObject} from './object-util.js';
 
 export type StringifyOptions = Partial<Options>;
 
@@ -30,30 +9,10 @@ export type StringifyOptions = Partial<Options>;
  * @returns {string}
  */
 export function stringify(input: unknown, options?: StringifyOptions): string {
-  let result = '';
-
   if (input === null || typeof input !== 'object') {
-    return result;
+    return '';
   }
 
   const optionsObj = options ?? {};
-  const {delimiter = defaultOptions.delimiter} = optionsObj;
-  const strDelimiter =
-    typeof delimiter === 'number' ? String.fromCharCode(delimiter) : delimiter;
-  const nestedValues = getNestedValues(input, optionsObj);
-  const keyLength = nestedValues.length;
-
-  for (let i = 0; i < keyLength; i++) {
-    const [key, value] = nestedValues[i];
-    const encodedKey = encodeString(String(key)) + '=';
-
-    if (i) {
-      result += strDelimiter;
-    }
-
-    result += encodedKey;
-    result += getAsPrimitive(value);
-  }
-
-  return result;
+  return stringifyObject(input as Record<PropertyKey, unknown>, optionsObj);
 }
